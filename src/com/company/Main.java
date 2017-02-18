ce791dd5ac623957129066465961b86290773de5
Marek Gilewski <marek.gilewski@outlook.com>
2017-02-18 16:07:49 +0100

    book class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,42 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    public static List<Book> bookList = new ArrayList<Book>();
 
     public static void main(String[] args) {
 
-        Book book = new Book();
-        System.out.println(book);
-
+        menu(bookList);
     }
+
+    public static void menu(List<Book> list){
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do{
+            System.out.println("Biblioteka");
+            System.out.println("--------------------");
+            System.out.println("1. Zobacz ksiązki");
+            System.out.println("2. Dodaj ksiązkę");
+            System.out.println("3. Usuń książkę");
+            System.out.println("4. Zakończ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Tytuł:");
+                    String title = sc.nextLine();
+                    
+                    list.add(new Book(title, ));
+                    break;
+            }
+
+        }while(choice!=4);
+    }
+
 }
